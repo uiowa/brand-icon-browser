c3091a74aace9379a3126b56b055347c5f6903bc
Dustin Quam <quamsta@gmail.com>
2022-05-31 11:40:18 -0500

    Pre rendering some routes to reduce jitter

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin-next");
+const PuppeteerRenderer = PrerenderSPAPlugin.PuppeteerRenderer;
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: (config) => {
+    // if (process.env.NODE_ENV !== "production") return;
+
+    return {
+      plugins: [
+        new PrerenderSPAPlugin({
+          // Required - The path to the webpack-outputted app to prerender.
+          staticDir: path.join(__dirname, "dist"),
+          // Required - Routes to render.
+          routes: ["/", "/category/home-personal/"],
+        }),
+      ],
+    };
+  },
+});
