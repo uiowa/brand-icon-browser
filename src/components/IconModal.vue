2fbf7ca37fe258e9d1cf665ac7498992af8c8d64
Dustin Quam <quamsta@gmail.com>
2022-06-10 09:11:34 -0500

    added uids buttons

--- src/components/IconModal.vue
+++ src/components/IconModal.vue
@@ -51,56 +51,41 @@
         </div>
         <div class="modal__icon-preview-wrapper">
           <div class="icon-preview__download">
-            <a
+            <uids-button
               :href="getIconSrc(icon.name, selectedVariant, 'svg').value"
-              class="uids-button"
-              v-if="getVariantFormat(selectedVariant, 'svg')">
-              <div class="uids-button__inner">
-                <span>SVG</span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                  <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-                  <path
-                    d="M144 480C64.47 480 0 415.5 0 336C0 273.2 40.17 219.8 96.2 200.1C96.07 197.4 96 194.7 96 192C96 103.6 167.6 32 256 32C315.3 32 367 64.25 394.7 112.2C409.9 101.1 428.3 96 448 96C501 96 544 138.1 544 192C544 204.2 541.7 215.8 537.6 226.6C596 238.4 640 290.1 640 352C640 422.7 582.7 480 512 480H144zM303 392.1C312.4 402.3 327.6 402.3 336.1 392.1L416.1 312.1C426.3 303.6 426.3 288.4 416.1 279C407.6 269.7 392.4 269.7 383 279L344 318.1V184C344 170.7 333.3 160 320 160C306.7 160 296 170.7 296 184V318.1L256.1 279C247.6 269.7 232.4 269.7 223 279C213.7 288.4 213.7 303.6 223 312.1L303 392.1z"
-                  />
-                </svg>
-              </div>
-            </a>
-            <a
+              v-if="getVariantFormat(selectedVariant, 'svg')"
+              color="tertiary"
+              :arrow="false"
+            >SVG
+            <i class="fas fa-download"></i>
+            </uids-button>
+            <uids-button
               :href="
                 getIconSrc(icon.name, selectedVariant, 'png', 'square').value
               "
-              class="uids-button"
               v-if="getVariantFormat(selectedVariant, 'png')"
               download
+              color="tertiary"
+              :arrow="false"
             >
-              <div class="uids-button__inner">
-                <span>PNG (1:1)</span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                  <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-                  <path
-                    d="M144 480C64.47 480 0 415.5 0 336C0 273.2 40.17 219.8 96.2 200.1C96.07 197.4 96 194.7 96 192C96 103.6 167.6 32 256 32C315.3 32 367 64.25 394.7 112.2C409.9 101.1 428.3 96 448 96C501 96 544 138.1 544 192C544 204.2 541.7 215.8 537.6 226.6C596 238.4 640 290.1 640 352C640 422.7 582.7 480 512 480H144zM303 392.1C312.4 402.3 327.6 402.3 336.1 392.1L416.1 312.1C426.3 303.6 426.3 288.4 416.1 279C407.6 269.7 392.4 269.7 383 279L344 318.1V184C344 170.7 333.3 160 320 160C306.7 160 296 170.7 296 184V318.1L256.1 279C247.6 269.7 232.4 269.7 223 279C213.7 288.4 213.7 303.6 223 312.1L303 392.1z"
-                  />
-                </svg>
-              </div>
-            </a>
-            <a
+              PNG (1:1)
+              <i class="fas fa-download"></i>
+            </uids-button>
+            <uids-button
               :href="
                 getIconSrc(icon.name, selectedVariant, 'png', 'wide').value
               "
               class="uids-button"
               v-if="getVariantFormat(selectedVariant, 'png')"
+              :arrow="false"
               download
+              color="tertiary"
             >
-              <div class="uids-button__inner">
-                <span>PNG (16:9)</span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                  <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-                  <path
-                    d="M144 480C64.47 480 0 415.5 0 336C0 273.2 40.17 219.8 96.2 200.1C96.07 197.4 96 194.7 96 192C96 103.6 167.6 32 256 32C315.3 32 367 64.25 394.7 112.2C409.9 101.1 428.3 96 448 96C501 96 544 138.1 544 192C544 204.2 541.7 215.8 537.6 226.6C596 238.4 640 290.1 640 352C640 422.7 582.7 480 512 480H144zM303 392.1C312.4 402.3 327.6 402.3 336.1 392.1L416.1 312.1C426.3 303.6 426.3 288.4 416.1 279C407.6 269.7 392.4 269.7 383 279L344 318.1V184C344 170.7 333.3 160 320 160C306.7 160 296 170.7 296 184V318.1L256.1 279C247.6 269.7 232.4 269.7 223 279C213.7 288.4 213.7 303.6 223 312.1L303 392.1z"
-                  />
-                </svg>
-              </div>
-            </a>
+       
+             PNG (16:9)
+              <i class="fas fa-download"></i>
+       
+            </uids-button>
           </div>
         </div>
         <div class="tags">
@@ -127,7 +112,7 @@
 <script setup>
 import { ref, defineProps } from "vue";
 import Icon from "@/components/Icon.vue";
-import UidsButton from "@/components/UidsButton.vue";
+import { UidsButton } from "uids";
 import UidsTag from "@/components/UidsTag.vue";
 import getIconSrc from "../composables/getIconSrc.js";
 
@@ -195,6 +180,7 @@ function closeModal() {
 </script>
 
 <style lang="scss">
+
 .modal {
   width: 75%;
   padding: 30px;
