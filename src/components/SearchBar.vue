edc0f0c39b026b863a9c4d36879cec5689fdb8ac
Dustin Quam <quamsta@users.noreply.github.com>
2022-07-07 13:50:09 -0500

    Added official UIDS buttons (#46)
    * added uids buttons
    
    * added outlines to buttons
    
    * added some additional styles that may be a good fit for uids
    
    * removed padding
    
    * button text and using uids buttons
    
    * updated to latest commit of 4.x-button-updates
    
    * removed extraneous attributes
    
    * cleaned up non-UIDS components, using small sized buttons
    
    * updated uids dependency

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -34,7 +34,7 @@
 <script setup>
 import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import UidsButton from "@/components/UidsButton.vue";
+
 const router = useRouter();
 const route = useRoute();
 const props = defineProps({
